8fc2b1dfa084689b455de3b8831fc44a9cace874
TooooooR <taras.protsiv12@gmail.com>
2024-02-26 14:05:56 +0200

    add to main

--- lab1/main/pumpkins.py
+++ lab1/main/pumpkins.py
@@ -4,20 +4,19 @@ field = [[1, 2, 3, 4],
          [13, 14, 15, 16]]
 
 
-def reverse_fonk(arr):
-    return arr[::-1]
-
-
 def find_robot_way(arr):
+    additional_list = []
     robot_way = []
-    m = len(arr)  # Кількість рядків в масиві
-    n = len(arr[0])  # Кількість стовпців в масиві
-    for i in range(m):
-        if i % 2 == 0:
-            robot_way.extend(arr[i])
-        else:
-            reversed_list = reverse_fonk(arr[i])
-            robot_way.extend(reversed_list)
+    strings = len(arr)
+    columns = len(arr[0])
+    for i in range(columns):
+        for j in range(strings):
+            additional_list.append(arr[-(j + 1)][-(i + 1)])
+        if i % 2 != 0:
+            additional_list.reverse()
+        robot_way.extend(additional_list)
+        additional_list = []
+
     return robot_way
 
 
